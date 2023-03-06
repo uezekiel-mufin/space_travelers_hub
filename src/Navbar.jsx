05631e02fab464141d2db07e2803cebe0010e171
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-03-06 16:15:28 +0100

    add navlinks and the view components

---
+++ src/Navbar.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  const navLinks = [
+    {
+      id: 1,
+      name: 'Rockets',
+      link: 'rockets',
+    },
+    {
+      id: 2,
+      name: 'Missions',
+      link: 'missions',
+    },
+    {
+      id: 3,
+      name: 'My Profile',
+      link: 'my-profile',
+    },
+  ];
+  return (
+    <header>
+      <div>
+        <img src="" alt="" />
+        <h2>Space Traveler&apos;s Hub</h2>
+      </div>
+      <div>
+        {navLinks.map((link) => (
+          <Link to={link.link} key={link.id}>
+            {link.name}
+          </Link>
+        ))}
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,42 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Navbar from './Navbar';
+import MyProfile from './components/Views/MyProfile';
+import Missions from './components/Views/Missions';
+import Rockets from './components/Views/Rockets';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Navbar />,
+    children: [
+      {
+        path: '/',
+        element: <App />,
+      },
+      {
+        path: '/my-profile',
+        element: <MyProfile />,
+      },
+      {
+        path: '/my-profile',
+        element: <Missions />,
+      },
+      {
+        path: '/my-profile',
+        element: <Rockets />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
