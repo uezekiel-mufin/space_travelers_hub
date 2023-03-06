ed95a72def28334a986b3a5f7474894343a0b2a3
mohi <mohisadeghi302@gmail.com>
2023-03-07 01:57:36 +0330

    Add slice

---
+++ src/redux/rucketSlice.jsx
@@ -0,0 +1,9 @@
+const initialState = {
+  rockets: [],
+};
+
+const rocketReducer = (state = initialState) => ({
+  ...state,
+});
+
+export default rocketReducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
+import rocketReducer from './rucketSlice';
 
 const store = configureStore({
   reducer: {
+    rockets: rocketReducer,
   },
 });
 
